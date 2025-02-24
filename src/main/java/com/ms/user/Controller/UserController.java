39a9fac264df5dad732bf767ea8ac3f9a2debf4b
ryan <ryantofanini@gmail.com>
2025-02-24 14:29:57 -0300

    first commit

---
+++ src/main/java/com/ms/user/Controller/UserController.java
@@ -0,0 +1,24 @@
+package com.ms.user.Controller;
+
+import com.ms.user.Dto.UserRecordDto;
+import com.ms.user.models.UserModel;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+
+    @PostMapping("/users")
+    public ResponseEntity<UserModel> createUser(@RequestBody @Valid UserRecordDto userRecordDto) {
+        var userModel = new UserModel();
+        BeanUtils.copyProperties(userRecordDto, userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body();
+    }
+}
